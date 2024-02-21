f677f31f869292e21fcdb6ff24c756872e4fcd06
Subilan <suyuanliang1995@gmail.com>
2024-02-22 00:43:07 +0800

    (main) centralize db connection

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package lab_backend
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"github.com/gin-gonic/gin"
+	"seatimc/lab-backend/utils"
+	"time"
+)
 
 func main() {
+	dbConf := Conf().Database
+	Db, err := sql.Open("mysql", dbConf.User+":"+dbConf.Password+"@"+dbConf.Host+"/"+dbConf.DbName)
+	utils.MustPanic(err)
+	Db.SetConnMaxLifetime(time.Minute * 3)
+	Db.SetMaxOpenConns(10)
+	Db.SetMaxIdleConns(10)
+
 	router := gin.New()
 	router.Use(middlewares())
 
