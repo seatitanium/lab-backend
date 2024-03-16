6548c0bf01e53ca090f6483f74d59b1189d23f0e
Subilan <suyuanliang1995@gmail.com>
2024-03-16 21:38:46 +0800

    chore: lines

--- handlers/auth/register.go
+++ handlers/auth/register.go
@@ -14,20 +14,24 @@ func HandleRegister(db *sqlx.DB) gin.HandlerFunc {
 			backend.RespondNg(c, "Invalid Request Body", "", nil)
 			return
 		}
+
 		tx := db.MustBegin()
 		hash, err := utils.GenerateHash(object.Password)
 		if err != nil {
 			backend.RespondNg(c, "Failed to generate hash for user: "+err.Error(), "", nil)
 			return
 		}
+
 		_, err = tx.Exec(
 			"INSERT INTO `seati_users` (`username`, `hash`, `mcid`, `email`) VALUES ($1, $2, $3, $4)",
 			object.Username, hash, object.MCID, object.Email,
 		)
+
 		if err != nil {
 			backend.RespondNg(c, "Failed to save user: "+err.Error(), "", nil)
 			return
 		}
+
 		backend.RespondOk(c, "Registered successfully.", "注册成功", nil)
 	}
 }
