4f2358035f82d24975c1549b589dce27def006c8
Subilan <suyuanliang1995@gmail.com>
2024-03-16 21:56:07 +0800

    fix: call

--- middleware.go
+++ middleware.go
@@ -15,13 +15,13 @@ func middlewares() gin.HandlerFunc {
 }
 
 func midfuncCheckOrigin(ctx *gin.Context) {
-	if currentOrigin := ctx.Request.Header.Get("Origin"); !slices.Contains(Conf().AllowedOrigins, currentOrigin) {
-		Respond(ctx, false, "Not supported origin", nil)
+	if currentOrigin := ctx.Request.Header.Get("Origin"); !slices.Contains(utils.Conf().AllowedOrigins, currentOrigin) {
+		utils.Respond(ctx, false, "Not supported origin", "", nil)
 	}
 }
 
 func midfuncAccessControl(ctx *gin.Context) {
-	ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*"+Conf().Domain)
+	ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*"+utils.Conf().Domain)
 	ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET")
@@ -31,6 +31,6 @@ func midfuncTokenCheck(ctx *gin.Context) {
 	checkErr := utils.CheckJWT(ctx.Request.Header.Get("Token"))
 
 	if checkErr != nil {
-		Respond(ctx, false, "Invalid credentials", nil)
+		utils.Respond(ctx, false, "Invalid credentials", "", nil)
 	}
 }
