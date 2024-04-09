aeb66f46a09cd4ad5927a83259f49c719ccbef5b
Subilan <suyuanliang1995@gmail.com>
2024-04-09 19:02:43 +0800

    doc

--- utils/booleans.go
+++ utils/booleans.go
@@ -2,6 +2,7 @@ package utils
 
 import "strings"
 
+// 根据 handlerName 判断该 handler 是否需要验证
 func NeedAuthorize(handlerName string) bool {
 	needs := Conf().NeedAuthorizeHandlers
 	for i := 0; i < len(needs); i++ {
