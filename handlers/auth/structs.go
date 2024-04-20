c2531b16c085edad3be3be3c67c74ac57c559204
Subilan <suyuanliang1995@gmail.com>
2024-04-20 19:54:05 +0800

    fix: make field required in binding

--- handlers/auth/structs.go
+++ handlers/auth/structs.go
@@ -2,13 +2,13 @@ package auth
 
 // 前端发来的用于注册的内容
 type RegisterRequest struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Email    string `json:"email"`
-	MCID     string `json:"mcid"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	MCID     string `json:"mcid" binding:"required"`
 }
 
 type LoginRequest struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
