d44c8210d72d0d12f895d9359dfc39fd8d8fe00f
Subilan <suyuanliang1995@gmail.com>
2024-04-22 17:49:01 +0800

    feat: stopped instance monitor

--- monitor/stopped-instance-monitor.go
+++ monitor/stopped-instance-monitor.go
@@ -19,6 +19,9 @@ import (
 func RunStoppedInstanceMonitor(interval time.Duration, threshold time.Duration, end <-chan bool) {
 	var stoppedDuration time.Duration
 
+	log.Printf("Stopped Instance Monitor\n")
+	log.Printf("Running with argument: interval=%v, threshold=%v\n", interval, threshold)
+
 	for {
 		var customErr *errHandler.CustomErr
 		var hasActiveInstance bool
@@ -26,6 +29,7 @@ func RunStoppedInstanceMonitor(interval time.Duration, threshold time.Duration, 
 		var retrieved *ecs.InstanceDescriptionRetrieved
 
 		hasActiveInstance, customErr = utils.HasActiveInstance()
+
 		if customErr != nil {
 			log.Println("Critical. Cannot determine active instance existence: " + customErr.Handle().Error())
 			goto endOfLoop
@@ -37,22 +41,35 @@ func RunStoppedInstanceMonitor(interval time.Duration, threshold time.Duration, 
 		}
 
 		activeInstance, customErr = utils.GetActiveInstance()
+
 		if customErr != nil {
 			log.Println("Critical. Cannot get active instance from database: " + customErr.Handle().Error())
 			goto endOfLoop
 		}
 
 		retrieved, customErr = ecs.DescribeInstance(activeInstance.InstanceId, activeInstance.RegionId)
+
 		if customErr != nil {
 			log.Println("Critical. Failed DescribeInstance: " + customErr.Handle().Error())
 			goto endOfLoop
 		}
 
+		if !retrieved.Exist {
+			log.Println("Skipped. No result retrieved.")
+			goto endOfLoop
+		}
+
 		if retrieved.Status == "Stopped" {
 			stoppedDuration += interval
+			log.Printf("Retrieved status \"Stopped\", adding stopped duration by %v.\n", interval)
+		} else {
+			stoppedDuration = 0
+			log.Printf("Retrieved status \"%v\", setting stopped duration to 0s.\n", retrieved.Status)
 		}
 
 		if stoppedDuration >= threshold {
+			log.Printf("Reaching the threshold of inactivity (%v). Forcefully deleting instance %v.\n", threshold, activeInstance.InstanceId)
+
 			customErr = ecs.DeleteInstance(activeInstance.InstanceId, true)
 			if customErr != nil {
 				log.Println("Critical. Failed DeleteInstance: " + customErr.Handle().Error())
@@ -64,6 +81,14 @@ func RunStoppedInstanceMonitor(interval time.Duration, threshold time.Duration, 
 				log.Println("Critical. Unable to write automated record: " + customErr.Handle().Error())
 				goto endOfLoop
 			}
+
+			customErr = utils.SetActive(activeInstance.InstanceId, false)
+			if customErr != nil {
+				log.Println("Critical. Unable to set instance active state.")
+				goto endOfLoop
+			}
+
+			log.Println("Finished. Successfully deleted instance.")
 		}
 
 	endOfLoop:
