8860a622583d5abf89ac9e2109a95508566846f4
Subilan <suyuanliang1995@gmail.com>
2024-04-29 16:53:54 +0800

    feat: describe invocation results

---
+++ aliyun/ecs/describe-invocation-results.go
@@ -0,0 +1,32 @@
+package ecs
+
+import (
+	ecs "github.com/alibabacloud-go/ecs-20140526/v4/client"
+	"github.com/alibabacloud-go/tea/tea"
+	"seatimc/backend/aliyun"
+	"seatimc/backend/errHandler"
+)
+
+// 获取指定 invokeId 的执行结果
+func DescribeInvocationResults(invokeId string) (string, *errHandler.CustomErr) {
+	client, customErr := aliyun.CreateEcsClient()
+
+	if customErr != nil {
+		return "", customErr
+	}
+
+	res, err := client.DescribeInvocationResults(&ecs.DescribeInvocationResultsRequest{
+		RegionId: tea.String(aliyun.AliyunConfig.PrimaryRegionId),
+		InvokeId: tea.String(invokeId),
+	})
+
+	if err != nil {
+		return "", errHandler.AliyunError(err)
+	}
+
+	for _, item := range res.Body.Invocation.InvocationResults.InvocationResult {
+		return tea.StringValue(item.Output), nil
+	}
+
+	return "", errHandler.ResNotExist()
+}
