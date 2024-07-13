8eecbaa75d8d4bb00918cf2c01d521fd23c91e6c
Subilan <suyuanliang1995@gmail.com>
2024-07-14 02:22:49 +0800

    chore: time parsing functions

--- utils/common.go
+++ utils/common.go
@@ -11,12 +11,20 @@ func MustPanic(mustPanic error) {
 	}
 }
 
-func ParseTimeATBZ(str string) (time.Time, error) {
+func ParseTime(layout string, str string) (time.Time, error) {
 	res, err := time.Parse(time.RFC3339, str)
 
 	if err == nil {
 		return res, nil
 	}
 
-	return time.Parse("2006-01-02T15:04Z", str)
+	return time.Parse(layout, str)
+}
+
+func ParseTimeRFC3339(str string) (time.Time, error) {
+	return ParseTime("2006-01-02T15:04:05Z", str)
+}
+
+func ParseTimeRFC3339Milli(str string) (time.Time, error) {
+	return ParseTime("2006-01-02T15:04:05.999Z07:00", str)
 }
