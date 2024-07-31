9164703ad203df1da16cd4f1f3d2e653de4b44d2
Subilan <suyuanliang1995@gmail.com>
2024-07-31 11:46:07 +0800

    fix: change line

--- build.sh
+++ build.sh
@@ -1,2 +1,1 @@
-env GOOS=linux GOARCH=amd64 TZ=Asia/Shanghai
-go build -ldflags="-X seatimc/backend/common.LastBuiltAt=$(date +%Y-%m-%d_%H:%M:%S)+08:00" -o lab
+env GOOS=linux GOARCH=amd64 TZ=Asia/Shanghai go build -ldflags="-X seatimc/backend/common.LastBuiltAt=$(date +%Y-%m-%d_%H:%M:%S)+08:00" -o lab
