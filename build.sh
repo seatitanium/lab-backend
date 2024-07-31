a937e30c3e7113e1c6dfdccb0e0ca9801300bd76
Subilan <suyuanliang1995@gmail.com>
2024-07-31 11:21:24 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-env GOOS=linux GOARCH=amd64 go build -o lab -ldflags='-X common.LastBuiltAt="date +%Y-%m-%d_%H:%M:%S"'
+env GOOS=linux GOARCH=amd64
+go build -o lab -ldflags='-X common.LastBuiltAt="date +%Y-%m-%d_%H:%M:%S"'
