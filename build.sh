2979cca4485e46eba1c6b8582411488737287dbe
Subilan <suyuanliang1995@gmail.com>
2024-07-31 12:37:22 +0800

    chore: change build time type

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 export TZ=Asia/Shanghai
-env GOOS=linux GOARCH=amd64 go build -ldflags="-X seatimc/backend/common.LastBuiltAt=$(date +%Y-%m-%d_%H:%M:%S)+08:00" -o lab
+env GOOS=linux GOARCH=amd64 go build -ldflags="-X seatimc/backend/common.LastBuiltAt=$(date +%Y%m%d_%H%M%S)" -o lab
