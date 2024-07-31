1c40b220182178ba072630a4085d78c900b6b514
Subilan <suyuanliang1995@gmail.com>
2024-07-31 11:34:09 +0800

    chore: remove buildnumber

--- build.sh
+++ build.sh
@@ -1,5 +1,2 @@
-n=$(cat buildnumber)
-nn=$((n+1))
 env GOOS=linux GOARCH=amd64 TZ=Asia/Shanghai
-go build -ldflags="-X seatimc/backend/common.LastBuiltAt=$nn.$(date +%Y-%m-%d_%H:%M:%S)+08:00" -o lab
-echo $nn > buildnumber
+go build -ldflags="-X seatimc/backend/common.LastBuiltAt=$(date +%Y-%m-%d_%H:%M:%S)+08:00" -o lab
