186fa9f0376309138169a0844ac61e6a567288b9
Subilan <suyuanliang1995@gmail.com>
2024-07-31 11:51:21 +0800

    fix: timezone

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-env GOOS=linux GOARCH=amd64 TZ=Asia/Shanghai go build -ldflags="-X seatimc/backend/common.LastBuiltAt=$(date +%Y-%m-%d_%H:%M:%S)+08:00" -o lab
+export TZ=Asia/Shanghai
+env GOOS=linux GOARCH=amd64 go build -ldflags="-X seatimc/backend/common.LastBuiltAt=$(date +%Y-%m-%d_%H:%M:%S)+08:00" -o lab
