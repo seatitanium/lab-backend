e41749593854cd4c596172dda81d4086c8561202
Subilan <suyuanliang1995@gmail.com>
2024-08-01 11:36:14 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 export TZ=Asia/Shanghai
-env GOOS=linux GOARCH=amd64 go build -ldflags="-X seatimc/backend/common.LastBuiltAt=$(date '+%Y-%m-%d %H:%M:%S')" -o lab
+current=$(date '+%Y-%m-%dT%H:%M:%SZ+08:00')
+env GOOS=linux GOARCH=amd64 go build -ldflags="-X seatimc/backend/common.LastBuiltAt=$current" -o lab
