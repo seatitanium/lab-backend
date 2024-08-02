9f2d8beaecae4c2826761dd19bedbaf15cd7b562
Subilan <suyuanliang1995@gmail.com>
2024-08-02 13:06:44 +0800

    feat: get available zone id

---
+++ aliyun/ecs/get-available-zone-id.go
@@ -0,0 +1,44 @@
+package ecs
+
+import (
+	client2 "github.com/alibabacloud-go/ecs-20140526/v4/client"
+	"github.com/alibabacloud-go/tea/tea"
+	"seatimc/backend/aliyun"
+	"seatimc/backend/errors"
+)
+
+func GetAvailableZoneId() (string, *errors.CustomErr) {
+	client, customErr := aliyun.CreateEcsClient()
+
+	if customErr != nil {
+		return "", customErr
+	}
+
+	resp, err := client.DescribeAvailableResource(&client2.DescribeAvailableResourceRequest{
+		RegionId:            tea.String(aliyun.AliyunConfig.PrimaryRegionId),
+		InstanceChargeType:  tea.String(aliyun.AliyunConfig.Using.InstanceChargeType),
+		SpotStrategy:        tea.String(aliyun.AliyunConfig.Using.SpotStrategy),
+		DestinationResource: tea.String("InstanceType"),
+		InstanceType:        tea.String(aliyun.AliyunConfig.Using.InstanceType),
+	})
+
+	if err != nil {
+		return "", errors.AliyunError(err)
+	}
+
+	zoneId := ""
+
+Main:
+	for _, z := range resp.Body.AvailableZones.AvailableZone {
+		for _, r := range z.AvailableResources.AvailableResource {
+			for _, rr := range r.SupportedResources.SupportedResource {
+				if tea.StringValue(rr.Status) == "Available" {
+					zoneId = tea.StringValue(z.ZoneId)
+					break Main
+				}
+			}
+		}
+	}
+
+	return zoneId, nil
+}
